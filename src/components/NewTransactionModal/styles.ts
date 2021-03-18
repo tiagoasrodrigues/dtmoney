d5e2ac60e3ee74e479dc6850eb231aeeb24f44a8
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-03-18 10:21:49 -0300

    button colors

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -58,34 +58,43 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: 'green' | 'red';
 }
+
+const colors = {
+  green: '#33cc95',
+  red: '#e52e4d'
+};
 
 export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
 
-    background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+    background: ${(props) => props.isActive
+    ? transparentize(0.9, colors[props.activeColor])
+    : 'transparent'
+  };
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+display: flex;
+align-items: center;
+justify-content: center;
 
-    transition: border-color 0.2s;
+transition: border - color 0.2s;
 
-    &:hover {
-      border-color: ${darken(0.1, `#d7d7d7`)}
-    }
+&:hover {
+  border-color: ${darken(0.1, `#d7d7d7`)}
+}
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+img {
+  width: 20px;
+  height: 20px;
+}
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+span {
+  display: inline - block;
+  margin-left: 1rem;
+  font-size: 1rem;
+  color: var(--text - title);
+}
 `;
