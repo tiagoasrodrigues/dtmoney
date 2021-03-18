22314c3848a50b53284b005da78491b66ece283d
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-03-18 09:20:23 -0300

    creating Type buttons

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -37,13 +38,50 @@ export const Container = styled.form`
     border-radius: 0.25rem;
     border: 0;
     font-size: 1rem;
-    margin-top: 1rem.5rem;
+    margin-top: 1.5rem;
     font-weight: 600;
 
     transition: filter 0.2s;
 
     &:hover {
       filter: brightness(0.9);
+    }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+
+  button {
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+
+    background: transparent;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: border-color 0.2s;
+
+    &:hover {
+      border-color: ${darken(0.1, `#d7d7d7`)}
+    }
+
+    img {
+      width: 20px;
+      height: 20px;
+    }
+
+    span {
+      display: inline-block;
+      margin-left: 1rem;
+      font-size: 1rem;
+      color: var(--text-title);
     }
   }
 `;
