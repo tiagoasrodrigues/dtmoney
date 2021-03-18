8747089907feeed50c59f3f448d0be7a34c06816
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-03-18 12:28:54 -0300

    listing transactions and seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-03-25 11:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-03-31 10:00:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
